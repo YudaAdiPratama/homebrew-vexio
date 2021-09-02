e594e8c01d2ab12bfab29adcbec65abf130b2e18
Yuda Adi Pratama <61916091+YudaAdiPratama@users.noreply.github.com>
2021-09-02 22:26:03 +0700

    Update vexio.rb

--- vexio.rb
+++ vexio.rb
@@ -1,35 +1,31 @@
-# typed: false
-# frozen_string_literal: true
-
 class Vexio < Formula
-  homepage "https://github.com/YudaAdiPratama/vexio"
-  url "https://github.com/YudaAdiPratama/vexio/releases/download/v1.7.0/vexio-1.7.0.mojave.bottle.tar.gz"
-  version "1.7.0"
-  revision 0
 
-  bottle do
-    root_url "https://github.com/YudaAdiPratama/vexio/releases/download/v1.7.0"
-    sha256 big_sur: "5dc376aa20241233b76e2ec2c1d4e862443a0250916b2838a1ff871e8a6dc2c5"
-  end
+   homepage "https://github.com/YudaAdiPratama/vexio/"
+   revision 0
+   url "https://github.com/YudaAdiPratama/vexio/archive/v1.7.0.tar.gz"
+   version "1.7.0"
+   
+   option :universal
+
+   depends_on "cmake" => :build
+   depends_on "automake" => :build
+   depends_on "libtool" => :build
+   depends_on "wget" => :build
+   depends_on "gmp" => :build
+   depends_on "gettext" => :build
+   depends_on "doxygen" => :build
+   depends_on "graphviz" => :build
+   depends_on "lcov" => :build
+   depends_on :xcode => :build
+   depends_on :macos => :high_sierra
+   depends_on :arch =>  :intel
   
-  bottle do
-    root_url "https://github.com/YudaAdiPratama/vexio/releases/download/v1.7.0"
-    sha256 mojave: "c3f66cdf5fb02469a9e8d11cb2637eb347ba44b2b68ef77d84bdb1401d6bb685"
-  end
-
-  bottle do
-    root_url "https://github.com/YudaAdiPratama/vexio/releases/download/v1.7.0"
-    sha256 catalina: "b1562e0e2d97288104839884871212d4c2ab1b54b0764aa9bb5f24bb1d5d37ab"
-  end
-  option :universal
-
-  depends_on arch: :intel
-  depends_on "gmp"
-  depends_on "libpqxx"
-  depends_on "libusb"
-  depends_on macos: :big_sur
-  depends_on "openssl@1.1"
-  def install
-    raise "Error, only supporting binary packages at this time"
-  end
+   bottle do
+      root_url "https://github.com/vexanium/vexio/releases/download/v1.7.0"
+      sha256 "44b9ab6e8d75212e888095e872cc101d2c4cc9d747d15696924868836e7988f9" => :mojave
+   end
+   def install
+      raise "Error, only supporting binary packages at this time"
+   end
 end
+__END__
