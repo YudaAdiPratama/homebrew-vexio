fb7b526c68b84953f6165de1a1eaa3928077a340
Yuda Adi Pratama <61916091+YudaAdiPratama@users.noreply.github.com>
2021-09-17 14:56:43 +0700

    Update vexio.rb

--- vexio.rb
+++ vexio.rb
@@ -1,6 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Vexio < Formula
   homepage "https://github.com/YudaAdiPratama/vexio"
   revision 0
