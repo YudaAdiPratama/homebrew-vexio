52e88673b9a3c518b8259792e8538a2aa5b13264
Yuda Adi Pratama <61916091+YudaAdiPratama@users.noreply.github.com>
2021-09-17 14:12:06 +0700

    Update vexio.rb

--- vexio.rb
+++ vexio.rb
@@ -1,26 +1,33 @@
-# typed: false
-# frozen_string_literal: true
-
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Vexio < Formula
-   homepage "https://github.com/YudaAdiPratama/vexio"
-   url "https://github.com/YudaAdiPratama/vexio/archive/v1.7.4.tar.gz"
-   version "1.7.0"
-   revision 0
- 
-   bottle do
-     root_url "https://github.com/YudaAdiPratama/vexio/releases/download/v1.7.0"
-     sha256 big_sur: "eab3fbc3688aecec0fe90b8d0fe3cb7beb84ed773ba0411fc2f855c66deaf882"
-   end
-   option :universal
- 
-   depends_on arch: :intel
-   depends_on "gmp"
-   depends_on "libpqxx"
-   depends_on "libusb"
-   depends_on macos: :big_sur
-   depends_on "openssl@1.1"
-   def install
-     raise "Error, only supporting binary packages at this time"
-   end
- end
-__END__
+  desc ""
+  homepage "https://github.com/YudaAdiPratama/vexio"
+  url "https://github.com/YudaAdiPratama/vexio/releases/download/v1.7.4/vexio-1.7.4.big_sur.bottle.tar.gz"
+  sha256 "f2a4c8c52b91ec1aecfe02a5de24a2192b4df3c570352c6b3df7a8a5494b4198"
+  license ""
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test vexio`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
