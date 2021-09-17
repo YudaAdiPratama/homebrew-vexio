5513e7a13b0d15dca432b55cd213b494b48a700a
Yuda Adi Pratama <61916091+YudaAdiPratama@users.noreply.github.com>
2021-09-17 14:45:01 +0700

    Update vexio.rb

--- vexio.rb
+++ vexio.rb
@@ -2,32 +2,32 @@
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Vexio < Formula
-  desc ""
   homepage "https://github.com/YudaAdiPratama/vexio"
+  revision 0
   url "https://github.com/YudaAdiPratama/vexio/releases/download/v1.7.4/vexio-1.7.4.big_sur.bottle.tar.gz"
-  sha256 "f2a4c8c52b91ec1aecfe02a5de24a2192b4df3c570352c6b3df7a8a5494b4198"
-  license ""
+  version "1.7.4"
 
-  # depends_on "cmake" => :build
+  option :universal
 
-  def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-    system "./configure", *std_configure_args, "--disable-silent-rules"
-    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
-  end
+  depends_on "cmake" => :build
+   depends_on "automake" => :build
+   depends_on "libtool" => :build
+   depends_on "wget" => :build
+   depends_on "gmp" => :build
+   depends_on "gettext" => :build
+   depends_on "doxygen" => :build
+   depends_on "graphviz" => :build
+   depends_on "lcov" => :build
+   depends_on :xcode => :build
+   depends_on :macos => :big_sur
+   depends_on :arch =>  :intel
 
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test vexio`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
-  end
+   bottle do
+      root_url "https://github.com/YudaAdiPratama/vexio/releases/download/v1.7.4"
+      sha256 "5dc376aa20241233b76e2ec2c1d4e862443a0250916b2838a1ff871e8a6dc2c5" => :big_sur
+   end
+   def install
+      raise "Error, only supporting binary packages at this time"
+   end
 end
+__END__
